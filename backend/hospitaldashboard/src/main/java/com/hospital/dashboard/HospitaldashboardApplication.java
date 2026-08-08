fcd2a3cc215b14019092265cdc99cf8a203182bf
swetha2006mkss-star <swetha2006mkss-star>
2026-08-08 22:10:34 +0530

    completebackend setup

---
+++ backend/hospitaldashboard/src/main/java/com/hospital/dashboard/HospitaldashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitaldashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitaldashboardApplication.class, args);
+	}
+
+}
